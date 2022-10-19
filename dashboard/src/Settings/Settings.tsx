139f83e580b4fad4cf6d29cecbffc73b0eaf467f
haidarhmd1 <haidar.hmd1@gmail.com>
2022-10-19 21:17:30 +0200

    remove dashboard as if it will be replaced with wp backend later

--- dashboard/src/Settings/Settings.tsx
+++
@@ -1,6 +0,0 @@
-import { Text } from '@mantine/core';
-import React from 'react';
-
-export function Settings() {
-  return <Text>Settings</Text>;
-}
--- dashboard/src/components/ExpressVisaTable/index.ts
+++
@@ -1,1 +0,0 @@
-export { ExpressVisaTable } from './ExpressVisaTable';
