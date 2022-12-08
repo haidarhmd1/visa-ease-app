5a55f4c74081e558b55fc32e12341b537102215e
haidarhmd1 <haidar.hmd1@gmail.com>
2022-12-08 11:55:14 +0100

    init migration from styled components to tailwinds nativewind for stylings

--- babel.config.js
+++ babel.config.js
@@ -1,18 +1,19 @@
 module.exports = function (api) {
-    api.cache(true);
-    return {
-        presets: ['babel-preset-expo'],
-        plugins: [
-            [
-                'module-resolver',
-                {
-                    root: ['./src'],
-                    alias: {
-                        test: './test',
-                        underscore: 'lodash',
-                    },
-                },
-            ],
-        ],
-    };
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      'nativewind/babel',
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          alias: {
+            test: './test',
+            underscore: 'lodash',
+          },
+        },
+      ],
+    ],
+  };
 };
