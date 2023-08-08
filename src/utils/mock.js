87b3e12a7a22c28794eb618355bb1fe7ed49421a
haidarhmd1 <haidar.hmd1@gmail.com>
2023-08-08 08:42:56 +0200

    add useNavigation into the global mock file

--- src/utils/mock.js
+++ src/utils/mock.js
@@ -2,3 +2,9 @@ import mockSafeAreaContext from 'react-native-safe-area-context/jest/mock';
 
 jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
 jest.mock('react-native-safe-area-context', () => mockSafeAreaContext);
+jest.mock('@react-navigation/native', () => {
+  return {
+    ...jest.requireActual('@react-navigation/native'),
+    useNavigation: jest.fn(() => ({})),
+  };
+});
